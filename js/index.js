c40b6b6a2a465dbdca0838d3f425bb75ea96a668
Trieu <trieu186168@gmail.com>
2024-01-26 13:22:47 +0700

    edit 6

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 var idEditTed = null;
 
 // ? bật loading
@@ -59,136 +57,130 @@ function fetchPhone() {
 }
 fetchPhone();
 
-
 // ! hàm dropdown
 function filterPhone() {
   var filterSelect = document.getElementById("filterPhone");
   var selectedType = filterSelect.value;
 
   if (selectedType === "all") {
-      fetchPhone(); // Nếu chọn "Tất cả sản phẩm", hiển thị toàn bộ danh sách
+    fetchPhone(); // Nếu chọn "Tất cả sản phẩm", hiển thị toàn bộ danh sách
   } else {
-      // Nếu chọn một loại cụ thể, lọc danh sách sản phẩm theo loại
-      turnOnLoading();
-      axios({
-          url: "https://65a5f6b274cf4207b4ef0ee4.mockapi.io/QLSP",
-          method: "GET",
+    // Nếu chọn một loại cụ thể, lọc danh sách sản phẩm theo loại
+    turnOnLoading();
+    axios({
+      url: "https://65a5f6b274cf4207b4ef0ee4.mockapi.io/QLSP",
+      method: "GET",
+    })
+      .then(function (res) {
+        turnOffLoading();
+        var filterPhone = res.data.filter(function (product) {
+          return product.type === selectedType;
+        });
+        renderPhone(filterPhone);
       })
-          .then(function (res) {
-              turnOffLoading();
-              var filterPhone = res.data.filter(function (product) {
-                  return product.type === selectedType;
-              });
-              renderPhone(filterPhone);
-          })
-          .catch(function (err) {
-              turnOffLoading();
-              console.log("error", err);
-          });
+      .catch(function (err) {
+        turnOffLoading();
+        console.log("error", err);
+      });
   }
 }
 
 // ! Giỏ hàng
 // Khai báo biến
 function getCartInfo() {
+  const cart = localStorage.getItem("cart");
+  if (cart == "undefined" || cart == null) {
+    return [];
+  }
 
-	const cart = localStorage.getItem("cart");
-	if (cart == "undefined" || cart == null) {
-		return [];
-	}
-	
-	return JSON.parse(cart);
+  return JSON.parse(cart);
 }
 
-function updateCartInfo(cart)
-{
-	localStorage.setItem("cart", JSON.stringify(cart));
+function updateCartInfo(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
-
 
 // Hàm để thêm sản phẩm vào giỏ hàng
 function addToCart(product) {
-	let cart = getCartInfo();
-	
-    // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
-    var existingItem = cart.find(function (item) {
-        return item.product.id === product.id;
-    });
+  let cart = getCartInfo();
 
-    if (existingItem) {
-        // Nếu sản phẩm đã tồn tại trong giỏ hàng, tăng số lượng
-        existingItem.quantity++;
-    } else {
-        // Nếu sản phẩm chưa tồn tại trong giỏ hàng, thêm vào giỏ hàng với số lượng là 1
-        var newItem = {
-            product: product,
-            quantity: 1,
-        };
-		
-        cart.push(newItem);
-    }
-	console.log(cart);
-	updateCartInfo(cart);
-    // Hiển thị thông tin giỏ hàng
-    displayCartInfo();
+  // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+  var existingItem = cart.find(function (item) {
+    return item.product.id === product.id;
+  });
+
+  if (existingItem) {
+    // Nếu sản phẩm đã tồn tại trong giỏ hàng, tăng số lượng
+    existingItem.quantity++;
+  } else {
+    // Nếu sản phẩm chưa tồn tại trong giỏ hàng, thêm vào giỏ hàng với số lượng là 1
+    var newItem = {
+      product: product,
+      quantity: 1,
+    };
+
+    cart.push(newItem);
+  }
+  console.log(cart);
+  updateCartInfo(cart);
+  // Hiển thị thông tin giỏ hàng
+  displayCartInfo();
 }
 
 // Hàm để hiển thị thông tin giỏ hàng
 function displayCartInfo() {
-    var cartInfo = document.getElementById("cartInfo");
-    var totalItems = 0;
-	const cart = getCartInfo();
-    // Tính tổng số lượng sản phẩm trong giỏ hàng
-    cart.forEach(function (item) {
-        totalItems += item.quantity;
-    });
+  var cartInfo = document.getElementById("cartInfo");
+  var totalItems = 0;
+  const cart = getCartInfo();
+  // Tính tổng số lượng sản phẩm trong giỏ hàng
+  cart.forEach(function (item) {
+    totalItems += item.quantity;
+  });
 
-    // Hiển thị thông tin giỏ hàng
-    cartInfo.innerHTML = "Giỏ hàng (" + totalItems + " sản phẩm)";
+  // Hiển thị thông tin giỏ hàng
+  cartInfo.innerHTML = " Giỏ hàng (" + totalItems + " sản phẩm)";
 }
-
 
 // Hàm khi người dùng bấm nút "Mua"
 function buyPhone(productId) {
-    // Tìm sản phẩm theo productId
-    var selectedProduct = null;
-    axios({
-        url: "https://65a5f6b274cf4207b4ef0ee4.mockapi.io/QLSP/" + productId,
-        method: "GET",
+  // Tìm sản phẩm theo productId
+  var selectedProduct = null;
+  axios({
+    url: "https://65a5f6b274cf4207b4ef0ee4.mockapi.io/QLSP/" + productId,
+    method: "GET",
+  })
+    .then(function (res) {
+      selectedProduct = res.data;
+
+      // Thêm sản phẩm vào giỏ hàng
+      addToCart(selectedProduct);
+
+      // Hiển thị thông báo cho người dùng (nếu cần)
+      alert("Sản phẩm đã được thêm vào giỏ hàng!");
     })
-        .then(function (res) {
-            selectedProduct = res.data;
-
-            // Thêm sản phẩm vào giỏ hàng
-            addToCart(selectedProduct);
-
-            // Hiển thị thông báo cho người dùng (nếu cần)
-            alert("Sản phẩm đã được thêm vào giỏ hàng!");
-        })
-        .catch(function (err) {
-            console.log("error", err);
-        });
+    .catch(function (err) {
+      console.log("error", err);
+    });
 }
-
 
 //! Hàm khi người dùng bấm nút "Giỏ hàng"
 function cartInfo() {
-    // Lấy thông tin từ giỏ hàng trong Local Storage
-    var cart = getCartInfo();
+  // Lấy thông tin từ giỏ hàng trong Local Storage
+  var cart = getCartInfo();
 
-    // Hiển thị thông tin sản phẩm trong modal
-    var tableBody = document.getElementById("thanhToan"); // Đây là tbody trong modal
-    var totalCell = document.getElementById("tinhTong"); // Đây là cell hiển thị tổng thanh toán
+  // Hiển thị thông tin sản phẩm trong modal
+  var tableBody = document.getElementById("thanhToan"); //  tbody trong modal
+  var totalCell = document.getElementById("tinhTong"); // hiển thị tổng thanh toán
 
-    // Reset nội dung của tbody
-    tableBody.innerHTML = "";
+  // Reset nội dung của tbody
+  tableBody.innerHTML = "";
 
-    // Duyệt qua từng sản phẩm trong giỏ hàng và cập nhật modal
-    cart.forEach(function (item, index) {
-        var tr = document.createElement("tr");
+  // Duyệt qua từng sản phẩm trong giỏ hàng và cập nhật modal
+  cart.forEach(function (item, index) {
+    var tr = document.createElement("tr");
 
-        // Cập nhật thông tin sản phẩm
-        tr.innerHTML = `
+    // Cập nhật thông tin sản phẩm
+    tr.innerHTML = `
         <tr>
             <td>${index + 1}</td>
             <td>${item.product.name}</td>
@@ -198,56 +190,70 @@ function cartInfo() {
             <button onclick="giamSL(${index})"> - </button>
             <span>${item.quantity}</span>
             <button onclick="tangSL(${index})"> + </button>
-        </td>
-            </tr>
+            </td>
+        </tr>
         `;
 
-        // Thêm dòng vào tbody
-        tableBody.appendChild(tr);
-    });
+    // Thêm dòng vào tbody
+    tableBody.appendChild(tr);
+  });
 
-    // Tính tổng thanh toán
-    var totalPayment = 0;
-    cart.forEach(function (item) {
-        totalPayment += item.product.price * item.quantity;
-    });
+  // Tính tổng thanh toán
+  var totalPayment = 0;
+  cart.forEach(function (item) {
+    totalPayment += item.product.price * item.quantity;
+  });
 
-    // Hiển thị tổng thanh toán
-    totalCell.innerHTML = ` ${totalPayment} $`;
+  // Hiển thị tổng thanh toán
+  totalCell.innerHTML = ` ${totalPayment} $`;
 }
 
 // Hàm tăng số lượng sản phẩm trong giỏ hàng
 function tangSL(index) {
-    var cart = getCartInfo();
-    cart[index].quantity++;
-    updateCartInfo(cart);
-    cartInfo(); // Cập nhật lại thông tin trong modal
+  var cart = getCartInfo();
+  cart[index].quantity++;
+  updateCartInfo(cart);
+  cartInfo(); // Cập nhật lại thông tin trong modal
 }
 
 // Hàm giảm số lượng sản phẩm trong giỏ hàng
 function giamSL(index) {
-    var cart = getCartInfo();
-    if (cart[index].quantity > 0) {
-        cart[index].quantity--;
-        updateCartInfo(cart);
-        cartInfo(); // Cập nhật lại thông tin trong modal
-    }
-}
-
-
-// Hàm xoá sản phẩm khỏi giỏ hàng
-function removeFromCart(index) {
-    var cart = getCartInfo();
-    cart.splice(index, 1);
+  var cart = getCartInfo();
+  if (cart[index].quantity > 0) {
+    cart[index].quantity--;
     updateCartInfo(cart);
     cartInfo(); // Cập nhật lại thông tin trong modal
-      // Kiểm tra xem giỏ hàng có rỗng không
-      if (cart.length === 0) {
-        // Nếu giỏ hàng rỗng, thực hiện hành động để thoát khỏi trang thanh toán
-        // Ví dụ chuyển hướng về trang chủ:
-        window.location.href = "index.html";
-    }
-    
+  }
 }
 
+// Hàm xoá sản phẩm khỏi giỏ hàng
+function removeCart(index) {
+  var cart = getCartInfo();
+  cart.splice(index, 1);
+  updateCartInfo(cart);
+  cartInfo(); // Cập nhật lại thông tin trong modal
+  // Kiểm tra xem giỏ hàng có rỗng không
+  if (cart.length === 0) {
+    // Nếu giỏ hàng rỗng, thoát khỏi trang thanh toán
+    //chuyển hướng về trang chủ:
+    window.location.href = "index.html";
+  }
+}
 
+// Hàm thanh toán
+
+function addProduct() {
+  // Hiển thị thông báo thanh toán thành công
+  var thongBaoTT = document.getElementById("thongBaoTT");
+  thongBaoTT.style.display = "block";
+
+      // Làm sạch giỏ hàng trên giao diện người dùn
+      var cart = getCartInfo(cart);
+      
+      cartInfo(cart); // Cập nhật lại thông tin trong modal
+  
+      // Hiển thị thông báo mua thành công và thoát ra
+      alert("Đặt hàng thành công!");
+      // chuyển hướng về trang chủ
+      window.location.href = "index.html";
+  }
